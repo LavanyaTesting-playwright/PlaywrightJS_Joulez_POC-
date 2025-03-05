5f3137104230dbc460b32472989483b00e85bdf7
LavanyaTesting-playwright <lavanya.yeturi@innocito.com>
2025-03-05 15:42:09 +0530

    Update indexpage.js

--- pages/ui_pages/indexpage.js
+++ pages/ui_pages/indexpage.js
@@ -1,6 +1,4 @@
 import { LoginPage } from "./loginpage";
 import { VehicleBooking } from "./vehicleBookingPage";
-// import { CheckoutPage } from "./checkoutpage";
-// import { SideBarValidation } from "./sidebarvalidation";
 
 export { VehicleBooking, LoginPage };
